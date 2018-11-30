8cf3448d9d88d47a3d7f2228220de8d87c867a55
Sumanmit Padhi <sumanmit.padhi@outlook.com>
2018-11-30 18:44:41 +0530

    Scroll to bottom if user is close to bottom.

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,18 @@
 var socket = io();                     //Tells server to open up connection between itself and client, and maintain running the socket.
+
+function scrollToBottom () {
+  var messages = jQuery("#messages");
+  var newMessage = messages.children("li:last-child");
+  var clientHeight = messages.prop("clientHeight");
+  var scrollTop = messages.prop("scrollTop");
+  var scrollHeight = messages.prop("scrollHeight");
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight)
+    messages.scrollTop(scrollHeight);
+}
+
 socket.on("connect", () => {
   console.log("Connected to server.");
 });
@@ -14,6 +28,7 @@ socket.on("newMessage", function (message) {
     createdAt: formattedTime
   });
   jQuery("#messages").append(html);
+  scrollToBottom();
   // var li = jQuery("<li></li>");
   // li.text(`${message.from} ${formattedTime} : ${message.text}`);
   // jQuery("#messages").append(li);
@@ -28,6 +43,7 @@ socket.on("newLocationMessage", message => {
     createdAt: formattedTime
   });
   jQuery("#messages").append(html);
+  scrollToBottom();
   // var li = jQuery("<li></li>");
   // var a = jQuery("<a target=\"_blank\">My current location</a>");
   // li.text(`${message.from} ${formattedTime}: `);
